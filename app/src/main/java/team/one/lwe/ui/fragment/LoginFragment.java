b30a6eb0c0ea0d54ee7da3f27878c0fa55861ae9
misterplus <40316318+misterplus@users.noreply.github.com>
2021-04-23 20:35:56 +0800

    修复#5
    Co-authored-by: chen2 <2397651454@qq.com>

--- app/src/main/java/team/one/lwe/ui/fragment/LoginFragment.java
+++ app/src/main/java/team/one/lwe/ui/fragment/LoginFragment.java
@@ -15,6 +15,7 @@ import androidx.fragment.app.Fragment;
 import com.netease.nim.uikit.api.NimUIKit;
 import com.netease.nim.uikit.common.ToastHelper;
 import com.netease.nim.uikit.common.ui.dialog.DialogMaker;
+import com.netease.nim.uikit.common.util.sys.NetworkUtil;
 import com.netease.nimlib.sdk.RequestCallback;
 import com.netease.nimlib.sdk.auth.LoginInfo;
 import org.jetbrains.annotations.NotNull;
@@ -43,6 +44,8 @@ public class LoginFragment extends Fragment {
             String password = editTextPassword.getText().toString();
             if (!isUsernameValid(username) || !isPasswordValid(password)) {
                 ToastHelper.showToast(view.getContext(), R.string.lwe_error_login_format);
+            } else if (!NetworkUtil.isNetAvailable(getActivity())){
+                ToastHelper.showToast(view.getContext(), R.string.lwe_error_nonetwork);
             } else {
                 DialogMaker.showProgressDialog(view.getContext(), inflater.getContext().getString(R.string.lwe_progress_login), false);
                 doConvert(username, password);
