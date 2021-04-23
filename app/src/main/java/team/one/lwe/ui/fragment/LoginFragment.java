ac9eb8bc0c989496bfb38a721719352039615ca2
misterplus <whiteplayz45@gmail.com>
2021-04-23 20:28:17 +0800

    Use uikit method instead

--- app/src/main/java/team/one/lwe/ui/fragment/LoginFragment.java
+++ app/src/main/java/team/one/lwe/ui/fragment/LoginFragment.java
@@ -15,6 +15,7 @@ import androidx.fragment.app.Fragment;
 import com.netease.nim.uikit.api.NimUIKit;
 import com.netease.nim.uikit.common.ToastHelper;
 import com.netease.nim.uikit.common.ui.dialog.DialogMaker;
+import com.netease.nim.uikit.common.util.sys.NetworkUtil;
 import com.netease.nimlib.sdk.RequestCallback;
 import com.netease.nimlib.sdk.auth.LoginInfo;
 import org.jetbrains.annotations.NotNull;
@@ -25,7 +26,6 @@ import team.one.lwe.bean.ASResponse;
 import team.one.lwe.network.NetworkThread;
 import team.one.lwe.util.APIUtils;
 import team.one.lwe.util.NavigationUtils;
-import team.one.lwe.util.StatusUtils;
 import team.one.lwe.util.TextUtils;
 
 public class LoginFragment extends Fragment {
@@ -44,7 +44,7 @@ public class LoginFragment extends Fragment {
             String password = editTextPassword.getText().toString();
             if (!isUsernameValid(username) || !isPasswordValid(password)) {
                 ToastHelper.showToast(view.getContext(), R.string.lwe_error_login_format);
-            } else if (!StatusUtils.isNetworkConnected(view.getContext())){
+            } else if (!NetworkUtil.isNetAvailable(getActivity())){
                 ToastHelper.showToast(view.getContext(), R.string.lwe_error_nonetwork);
             } else {
                 DialogMaker.showProgressDialog(view.getContext(), inflater.getContext().getString(R.string.lwe_progress_login), false);
