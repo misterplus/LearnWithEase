c374b7191254ede778505a0b6abefb436e1547bb
misterplus <whiteplayz45@gmail.com>
2021-04-27 20:07:37 +0800

    Util changes

--- app/src/main/java/team/one/lwe/util/APIUtils.java
+++ app/src/main/java/team/one/lwe/util/APIUtils.java
@@ -1,22 +1,23 @@
 package team.one.lwe.util;
 
 import cn.hutool.core.io.IORuntimeException;
+import cn.hutool.json.JSONUtil;
 import lombok.NonNull;
 import team.one.lwe.bean.ASResponse;
+import team.one.lwe.bean.User;
 
 public class APIUtils {
 
     public static ASResponse convert(@NonNull String username, @NonNull String password) throws IORuntimeException {
-        return PostUtils.doPost(
+        return PostUtils.doPostEncoded(
                 "/user/convert", 5000,
                 "username", username,
                 "password", password);
     }
 
-    public static ASResponse register(@NonNull String username, @NonNull String password) throws IORuntimeException {
-        return PostUtils.doPost(
+    public static ASResponse register(@NonNull User user) throws IORuntimeException {
+        return PostUtils.doPostJson(
                 "/user/register", 5000,
-                "username", username,
-                "password", password);
+                JSONUtil.toJsonStr(user));
     }
 }
