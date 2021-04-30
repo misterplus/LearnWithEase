193fde5af9a6324d78adce9e36661ccfd88fcecb
chen2 <2397651454@qq.com>
2021-04-30 17:32:48 +0800

    点击空白处收回键盘&username回车到密码

--- app/src/main/java/team/one/lwe/ui/activity/LoginActivity.java
+++ app/src/main/java/team/one/lwe/ui/activity/LoginActivity.java
@@ -1,11 +1,16 @@
 package team.one.lwe.ui.activity;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 
 import com.netease.nim.uikit.common.activity.UI;
 
 import team.one.lwe.R;
 import team.one.lwe.ui.fragment.LoginFragment;
+import team.one.lwe.util.TextUtils;
 
 public class LoginActivity extends UI {
 
@@ -20,5 +25,18 @@ public class LoginActivity extends UI {
                     .add(R.id.container, new LoginFragment())
                     .commit();
         }
+    }
+    @Override
+    public boolean dispatchTouchEvent(MotionEvent ev) {
+        switch (ev.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+                View view = getCurrentFocus();
+                TextUtils.hideKeyboard(ev, view, LoginActivity.this);
+                break;
+
+            default:
+                break;
+        }
+        return super.dispatchTouchEvent(ev);
     }
 }
