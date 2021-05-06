db7a5ccfa21272d0de3e4068ae9ae5d8683b3a5a
misterplus <40316318+misterplus@users.noreply.github.com>
2021-05-06 13:46:44 +0800

    [新内容]"我的"页面、app图标、软件设置页面、关于页面 (#10)
    Coauthored-by: Le0nidas-wow <964923436@qq.com>

---
+++ app/src/main/java/team/one/lwe/ui/fragment/PermissionsettingFragment.java
@@ -0,0 +1,30 @@
+package team.one.lwe.ui.fragment;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.netease.nim.uikit.common.activity.UI;
+
+import team.one.lwe.R;
+import team.one.lwe.ui.wedget.LWEToolBarOptions;
+
+public class PermissionsettingFragment extends Fragment {
+
+    private View view;
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        //TODO: finish register fragment
+        view = inflater.inflate(R.layout.fragment_permissionsetting, container, false);
+        LWEToolBarOptions options = new LWEToolBarOptions(R.string.permissionsetting);
+        ((UI) getActivity()).setToolBar(view, R.id.toolbar, options);
+        return view;
+    }
+}
