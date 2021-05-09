434758e3bc9765d3edbec73b28e2bef880ad49ae
chen2 <2397651454@qq.com>
2021-05-09 11:55:33 +0800

    添加昵称检查

--- app/src/main/java/team/one/lwe/ui/fragment/RegisterFragment.java
+++ app/src/main/java/team/one/lwe/ui/fragment/RegisterFragment.java
@@ -225,6 +225,8 @@ public class RegisterFragment extends Fragment {
                 ToastHelper.showToast(view.getContext(), R.string.lwe_error_login_format);
             } else if (!password.equals(confirmPassword)) {
                 ToastHelper.showToast(view.getContext(), R.string.lwe_error_confirm_password);
+            } else if (name.length() == 0 || name.length() > 16) {
+                ToastHelper.showToast(view.getContext(), R.string.lwe_error_name);
             } else if (age < 1 || age > 120) {
                 ToastHelper.showToast(view.getContext(), R.string.lwe_error_age);
             } else if (TextUtils.isEmpty(cPickerNames[0]) || TextUtils.isEmpty(cPickerNames[1]) || TextUtils.isEmpty(cPickerNames[2])) {
